d62c44feec632fea11a32480b0f2a303946b3329
callmemoe19 <jamilnashrulloh19@gmail.com>
2024-12-14 15:49:37 +0700

    first

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('alertButton').addEventListener('click', function() {
+        alert('Button was clicked!');
+    });
+});
